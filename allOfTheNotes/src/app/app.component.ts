c1fbb31c1bcd9f9f18e30ff4fe1ece718fe0bc74
DA M1 36 <dam136@DAs-MacBook-Air.local>
2024-05-10 15:07:24 +0200

    stashing changes

--- allOfTheNotes/src/app/app.component.ts
+++ allOfTheNotes/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NotesPageComponent } from './pages/notes-page/notes-page.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
+  standalone: true, 
+  imports: [RouterOutlet, NotesPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
