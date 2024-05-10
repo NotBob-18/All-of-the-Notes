1c7c83d7def89faf9b14d138c4cd73a7cd57a6e3
Samkelisiwe Mkhize <samkelsiwemkhize@gmail.com>
2024-05-10 14:47:38 +0200

    html

--- allOfTheNotes/src/app/pages/notes-page/notes-page.component.ts
+++ allOfTheNotes/src/app/pages/notes-page/notes-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-notes-page',
@@ -9,8 +10,8 @@ import { Component } from '@angular/core';
 })
 export class NotesPageComponent {
 
-
-
+   
   
-  
-}
+    
+  }
+ 
